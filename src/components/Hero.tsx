1dcbb03724653fbc8dfd381e20bb1bfb877fbbdb
William Oliveira <48337511+willraoli@users.noreply.github.com>
2023-07-10 20:38:25 -0300

    Refactor `Hero` section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,32 +1,27 @@
-import * as S from '@/styles/Hero.styles'
-import LinkButton from '@/components/shared/LinkButton'
-import musicAudioPlay from 'public/icons/music-audio-play.svg'
+import * as S from "@/styles/Hero.styles";
+import LinkButton from "@/components/shared/LinkButton";
+import musicAudioPlay from "@/assets/icons/music-audio-play.svg";
+import HeroRight from "@/components/HeroRight";
+import CTAButton from "@/components/shared/CTAButton";
 
 export default function Hero() {
-	return (
-		<S.Hero>
-			<S.HeroCTA>
-				<S.PlatformDescription>Plataforma All In One</S.PlatformDescription>
-				<S.Heading>Sua escola online poderosa e lucrativa</S.Heading>
-				<S.Subheading>
-					Tenha sua própria escola online 100% white label com rede social, gamificação, clube de assinaturas, ecommerce
-					e sistema EAD completo.
-				</S.Subheading>
-				<S.ButtonsContainer>
-					<LinkButton
-						href='/'
-						$variant='filled'
-					>
-						Começar agora
-					</LinkButton>
-					<LinkButton
-						href='/'
-						icon={musicAudioPlay}
-					>
-						Ver vídeo
-					</LinkButton>
-				</S.ButtonsContainer>
-			</S.HeroCTA>
-		</S.Hero>
-	)
+  return (
+    <S.HeroContainer>
+      <S.HeroLeft>
+        <S.PlatformOverline>Plataforma All In One</S.PlatformOverline>
+        <S.Heading>Sua escola online poderosa e lucrativa</S.Heading>
+        <S.Subheading>
+          Tenha sua própria escola online 100% white label com rede social,
+          gamificação, clube de assinaturas, ecommerce e sistema EAD completo.
+        </S.Subheading>
+        <S.ButtonsContainer>
+          <CTAButton href="/">Começar agora</CTAButton>
+          <LinkButton href="/" icon={musicAudioPlay}>
+            Ver vídeo
+          </LinkButton>
+        </S.ButtonsContainer>
+      </S.HeroLeft>
+      <HeroRight />
+    </S.HeroContainer>
+  );
 }
