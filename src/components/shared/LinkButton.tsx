9a58278c25e853bc43b073c1172ebe75c8c6363a
William Oliveira <48337511+willraoli@users.noreply.github.com>
2023-07-10 20:38:10 -0300

    Refactor `LinkButton`

--- src/components/shared/LinkButton.tsx
+++ src/components/shared/LinkButton.tsx
@@ -1,26 +1,18 @@
-import * as S from '@/styles/shared/LinkButton.styles'
-import { LinkButtonVariantProps } from '@/styles/shared/LinkButton.styles'
-import Image from 'next/image'
-import { PropsWithChildren } from 'react'
+import * as S from "@/styles/shared/LinkButton.styles";
+import { LinkButtonVariantProps } from "@/styles/shared/LinkButton.styles";
+import Image from "next/image";
+import { PropsWithChildren } from "react";
 
 interface LinkButtonProps extends PropsWithChildren, LinkButtonVariantProps {
-	icon?: string
-	href: string
+  icon?: string;
+  href: string;
 }
 
 export default function LinkButton(props: LinkButtonProps) {
-	return (
-		<S.LinkButton
-			$variant={props.$variant}
-			href={props.href}
-		>
-			{props.icon && (
-				<Image
-					src={props.icon}
-					alt='Icon'
-				/>
-			)}
-			{props.children}
-		</S.LinkButton>
-	)
+  return (
+    <S.LinkButton $variant={props.$variant} href={props.href}>
+      {props.icon && <Image src={props.icon} alt="Icon" />}
+      {props.children}
+    </S.LinkButton>
+  );
 }
--- src/styles/shared/LinkButton.styles.ts
+++ src/styles/shared/LinkButton.styles.ts
@@ -1,28 +1,27 @@
-import { Inter } from 'next/font/google'
-import Link from 'next/link'
-import styled from 'styled-components'
+"use client";
 
-const inter = Inter({ subsets: ['latin'] })
-
-const paddingHelper = {
-	outlined: '0.75rem 1.5rem',
-	filled: '1.12rem 2rem',
-}
+import Link from "next/link";
+import styled from "styled-components";
 
 export interface LinkButtonVariantProps {
-	$variant?: 'outlined' | 'filled'
+  $variant?: "outlined";
 }
 
 export const LinkButton = styled(Link)<LinkButtonVariantProps>`
-	border-radius: 5rem;
-	border: ${props => (props.$variant === 'outlined' ? '1px solid #fff' : 'none')};
-	background: ${props => (props.$variant === 'filled' ? '#00E1E7' : 'none')};
-	padding: ${props => (props.$variant ? paddingHelper[props.$variant] : 'none')};
-	font-family: ${inter.style.fontFamily};
-	font-weight: 600;
-	color: ${props => (props.$variant === 'filled' ? '#130C25' : '#fff')};
-	display: flex;
-	gap: 0.5rem;
-	align-items: center;
-	text-decoration: none;
-`
+  border-radius: 5rem;
+  border: ${(props) =>
+    props.$variant === "outlined" ? "1px solid #fff" : "none"};
+  padding: ${(props) =>
+    props.$variant === "outlined" ? "0.75rem 1.5rem" : "none"};
+  font-weight: 600;
+  color: #fff;
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+  text-decoration: none;
+  justify-content: center;
+
+  @media (max-width: 1130px) {
+    display: none;
+  }
+`;
