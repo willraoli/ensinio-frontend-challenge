ab1cce0b9a558abdff667b4b7b87e2f59a184bae
William Oliveira <48337511+willraoli@users.noreply.github.com>
2023-07-10 20:39:42 -0300

    Add `LocalizationDropdown`

--- src/components/LocalizationDropdown.tsx
+++ src/components/LocalizationDropdown.tsx
@@ -1,18 +1,57 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import Image from "next/image";
+import brazilFlag from "@/assets/icons/brazil-flag.svg";
+import spainFlag from "@/assets/icons/spain-flag.svg";
+import usaFlag from "@/assets/icons/usa-flag.svg";
 
 export default function LocalizationDropdown() {
-	const LOCALE_LIST = ['PT', 'EN', 'FR']
+  const LOCALE_LIST = [
+    {
+      label: "PT",
+      icon: brazilFlag,
+    },
+    {
+      label: "EN",
+      icon: usaFlag,
+    },
+    {
+      label: "ES",
+      icon: spainFlag,
+    },
+  ];
 
-	return <DropdownMenu>asdfasdfasdfasdf</DropdownMenu>
+  return (
+    <DropdownMenu>
+      {LOCALE_LIST.map((item, idx) => (
+        <LocalizationItem key={idx}>
+          <Image src={item.icon} alt={`${item.label} flag`} />
+          {item.label}
+        </LocalizationItem>
+      ))}
+    </DropdownMenu>
+  );
 }
 
+export const LocalizationItem = styled.div`
+  display: flex;
+  align-items: center;
+  color: #5a5566;
+  background-color: #fff;
+  gap: 0.5rem;
+  padding: 1rem 1.5rem;
+
+  &:hover {
+    background: linear-gradient(90deg, #efecfb 0%, #fff 100%);
+    cursor: pointer;
+  }
+`;
+
 export const DropdownMenu = styled.div`
-	position: absolute;
-	width: 8.625rem;
-	height: 9rem;
-	top: 2.5rem;
-	left: 50%;
-	transform: translateX(-50%);
-	border-radius: 0.5rem;
-	background-color: #fff;
-`
+  position: absolute;
+  top: 2.5rem;
+  left: 50%;
+  transform: translateX(-50%);
+  background - color: #fff;
+  border-radius: 0.5rem;
+  overflow: hidden;
+`;
