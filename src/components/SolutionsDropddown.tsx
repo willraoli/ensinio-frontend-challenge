8ea60c2accd991ac74606b78b667111c1ef5ec2b
William Oliveira <48337511+willraoli@users.noreply.github.com>
2023-07-10 20:39:25 -0300

    Add `SolutionsDropdown`

--- src/components/SolutionsDropddown.tsx
+++ src/components/SolutionsDropddown.tsx
@@ -1,38 +1,52 @@
-import * as S from '@/styles/SolutionsDropdown.styles'
-import eadIcon from 'public/icons/ead.svg'
-import socialIcon from 'public/icons/social.svg'
-import gamificationIcon from 'public/icons/gamification.svg'
-import appIcon from 'public/icons/app.svg'
-import Image from 'next/image'
+import * as S from "@/styles/SolutionsDropdown.styles";
+import eadIcon from "@/assets/icons/ead.svg";
+import socialIcon from "@/assets/icons/social.svg";
+import gamificationIcon from "@/assets/icons/gamification.svg";
+import appIcon from "@/assets/icons/app.svg";
+import Image from "next/image";
 
 export default function SolutionsDropdown() {
-	const GRID_ITEMS = [
-		{ title: 'Crie uma Escola Online', description: 'Lorem ipsum dolor sit amet', icon: eadIcon, href: '/' },
-		{ title: 'Gamificação', description: 'Lorem ipsum dolor sit amet', icon: gamificationIcon, href: '/' },
-		{ title: 'Comunidade e rede social', description: 'Lorem ipsum dolor sit amet', icon: socialIcon, href: '/' },
-		{ title: 'Aplicativo mobile', description: 'Lorem ipsum dolor sit amet', icon: appIcon, href: '/' },
-	]
+  const GRID_ITEMS = [
+    {
+      title: "Crie uma Escola Online",
+      description: "Lorem ipsum dolor sit amet",
+      icon: eadIcon,
+      href: "/",
+    },
+    {
+      title: "Gamificação",
+      description: "Lorem ipsum dolor sit amet",
+      icon: gamificationIcon,
+      href: "/",
+    },
+    {
+      title: "Comunidade e rede social",
+      description: "Lorem ipsum dolor sit amet",
+      icon: socialIcon,
+      href: "/",
+    },
+    {
+      title: "Aplicativo mobile",
+      description: "Lorem ipsum dolor sit amet",
+      icon: appIcon,
+      href: "/",
+    },
+  ];
 
-	return (
-		<S.DropdownMenu>
-			<S.SolutionsHeading>soluções principais</S.SolutionsHeading>
-			<S.Grid>
-				{GRID_ITEMS.map((item, idx) => (
-					<S.GridItem
-						key={idx}
-						href={item.href}
-					>
-						<Image
-							src={item.icon}
-							alt='Icon'
-						/>
-						<div>
-							<S.GridItemTitle>{item.title}</S.GridItemTitle>
-							<S.GridItemDescription>{item.description}</S.GridItemDescription>
-						</div>
-					</S.GridItem>
-				))}
-			</S.Grid>
-		</S.DropdownMenu>
-	)
+  return (
+    <S.DropdownMenu>
+      <S.SolutionsHeading>soluções principais</S.SolutionsHeading>
+      <S.Grid>
+        {GRID_ITEMS.map((item, idx) => (
+          <S.GridItem key={idx} href={item.href}>
+            <Image src={item.icon} alt="Icon" />
+            <div>
+              <S.GridItemTitle>{item.title}</S.GridItemTitle>
+              <S.GridItemDescription>{item.description}</S.GridItemDescription>
+            </div>
+          </S.GridItem>
+        ))}
+      </S.Grid>
+    </S.DropdownMenu>
+  );
 }
