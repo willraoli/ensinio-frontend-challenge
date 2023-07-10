b76b98164008112e39f5177f429e6413cc09a87d
William Oliveira <48337511+willraoli@users.noreply.github.com>
2023-07-10 20:37:58 -0300

    Add `HoverLink`

--- src/components/shared/HoverLink.tsx
+++ src/components/shared/HoverLink.tsx
@@ -1,31 +1,26 @@
-import { PropsWithChildren, useState } from 'react'
-import * as S from '@/styles/shared/HoverLink.styles'
-import downIcon from 'public/icons/down.svg'
-import Image from 'next/image'
+import { PropsWithChildren, useState } from "react";
+import * as S from "@/styles/shared/HoverLink.styles";
+import downIcon from "@/assets/icons/down.svg";
+import Image from "next/image";
 
 interface HoverLinkProps extends PropsWithChildren {
-	icon?: boolean
-	showOnHover: React.ReactNode
+  icon?: boolean;
+  showOnHover: React.ReactNode;
 }
 
 export default function HoverLink(props: HoverLinkProps) {
-	const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
 
-	return (
-		<S.HoverLinkContainer
-			onMouseOverCapture={() => setIsOpen(true)}
-			onMouseOutCapture={() => setIsOpen(false)}
-		>
-			<S.HoverLink href='/'>
-				{props.children}
-				{props.icon && (
-					<Image
-						src={downIcon}
-						alt='Down arrow'
-					/>
-				)}
-			</S.HoverLink>
-			{isOpen && props.showOnHover}
-		</S.HoverLinkContainer>
-	)
+  return (
+    <S.HoverLinkContainer
+      onMouseOverCapture={() => setIsOpen(true)}
+      onMouseOutCapture={() => setIsOpen(false)}
+    >
+      <S.HoverLink href="/">
+        {props.children}
+        {props.icon && <Image src={downIcon} alt="Down arrow" />}
+      </S.HoverLink>
+      {isOpen && props.showOnHover}
+    </S.HoverLinkContainer>
+  );
 }
