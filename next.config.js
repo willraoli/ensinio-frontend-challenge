967cf0e60ecd6443b9af16654e2398b4455f5574
William Oliveira <48337511+willraoli@users.noreply.github.com>
2023-07-11 16:13:39 -0300

    Add localization config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  i18n: {
+    locales: ["pt-BR", "en-US", "es"],
+    defaultLocale: "pt-BR",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
