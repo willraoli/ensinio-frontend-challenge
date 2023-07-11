6ce457d5d86df6f6258cbd6d5148ab265347e6c5
William Oliveira <48337511+willraoli@users.noreply.github.com>
2023-07-11 15:23:22 -0300

    Refactor `LinkButton`

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,33 +1,17 @@
-import HoverLink from "@/components/shared/HoverLink";
-import logo from "@/assets/logo.svg";
 import user from "@/assets/icons/user.svg";
-import Image from "next/image";
-import * as S from "@/styles/Navbar.styles";
-import Link from "next/link";
-import LinkButton from "@/components/shared/LinkButton";
-import SolutionsDropdown from "@/components/SolutionsDropddown";
-import LocalizationDropdown from "@/components/LocalizationDropdown";
+import logo from "@/assets/logo.svg";
 import Hamburger from "@/components/Hamburger";
+import LocalizationDropdown from "@/components/LocalizationDropdown";
+import SolutionsDropdown from "@/components/SolutionsDropddown";
+import HoverLink from "@/components/shared/HoverLink";
+import LinkButton from "@/components/shared/LinkButton";
+import * as S from "@/styles/Navbar.styles";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 export default function Navbar() {
-  const NAV_ITEMS = [
-    {
-      label: "Preços",
-      href: "/",
-    },
-    {
-      label: "Academy",
-      href: "/",
-    },
-    {
-      label: "Blog",
-      href: "/",
-    },
-    {
-      label: "Contato",
-      href: "/",
-    },
-  ];
+  const [open, setOpen] = useState(false);
 
   return (
     <S.Navbar>
@@ -39,11 +23,10 @@ export default function Navbar() {
           <HoverLink icon showOnHover={<SolutionsDropdown />}>
             Soluções
           </HoverLink>
-          {NAV_ITEMS.map((item, idx) => (
-            <S.NavLink href={item.href} key={idx}>
-              {item.label}
-            </S.NavLink>
-          ))}
+          <S.NavLink href="/">Preços</S.NavLink>
+          <S.NavLink href="/">Academy</S.NavLink>
+          <S.NavLink href="/">Blog</S.NavLink>
+          <S.NavLink href="/">Contato</S.NavLink>
           <S.Separator />
           <LinkButton href="/" icon={user}>
             Entrar
@@ -55,7 +38,23 @@ export default function Navbar() {
             PT
           </HoverLink>
         </S.Navigation>
-        <Hamburger />
+        <S.MobileNavigation $open={open}>
+          <S.MobileNavList>
+            <S.NavLink href={"/"}>Preços</S.NavLink>
+            <S.NavLink href={"/"}>Academy</S.NavLink>
+            <S.NavLink href={"/"}>Blog</S.NavLink>
+            <S.NavLink href={"/"}>Contato</S.NavLink>
+            <S.MobileSelect>
+              <option value="1">PT</option>
+              <option value="2">EN</option>
+              <option value="3">ES</option>
+            </S.MobileSelect>
+            <LinkButton href="/" icon={user}>
+              Entrar
+            </LinkButton>
+          </S.MobileNavList>
+        </S.MobileNavigation>
+        <Hamburger open={open} onClick={() => setOpen(!open)} />
       </S.NavbarWrapper>
     </S.Navbar>
   );
--- src/styles/shared/LinkButton.styles.ts
+++ src/styles/shared/LinkButton.styles.ts
@@ -8,6 +8,10 @@ export interface LinkButtonVariantProps {
 }
 
 export const LinkButton = styled(Link)<LinkButtonVariantProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
   border-radius: 5rem;
   border: ${(props) =>
     props.$variant === "outlined" ? "1px solid #fff" : "none"};
@@ -15,13 +19,5 @@ export const LinkButton = styled(Link)<LinkButtonVariantProps>`
     props.$variant === "outlined" ? "0.75rem 1.5rem" : "none"};
   font-weight: 600;
   color: #fff;
-  display: flex;
-  gap: 0.5rem;
-  align-items: center;
   text-decoration: none;
-  justify-content: center;
-
-  @media (max-width: 1130px) {
-    display: none;
-  }
 `;
