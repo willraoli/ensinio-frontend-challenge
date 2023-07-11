1f4ab17b4f0cff4c77b1e199d890596dcd0418ad
William Oliveira <48337511+willraoli@users.noreply.github.com>
2023-07-11 16:14:11 -0300

    Remove unnecessary imports

--- src/styles/shared/HoverLink.styles.ts
+++ src/styles/shared/HoverLink.styles.ts
@@ -1,23 +1,19 @@
-import { Inter } from 'next/font/google'
-import Link from 'next/link'
-import { styled } from 'styled-components'
-
-const inter = Inter({ subsets: ['latin'] })
+import Link from "next/link";
+import { styled } from "styled-components";
 
 export const HoverLink = styled(Link)`
-	text-decoration: none;
-	color: #fff;
-	font-size: 0.9375em;
-	font-style: normal;
-	line-height: 105%;
-	font-family: ${inter.style.fontFamily};
-	font-weight: 500;
-	display: flex;
-	gap: 0.5rem;
-	align-items: center;
-	height: 2.5rem;
-`
+  text-decoration: none;
+  color: #fff;
+  font-size: 0.9375em;
+  font-style: normal;
+  line-height: 105%;
+  font-weight: 500;
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+  height: 2.5rem;
+`;
 
 export const HoverLinkContainer = styled.div`
-	position: relative;
-`
+  position: relative;
+`;
